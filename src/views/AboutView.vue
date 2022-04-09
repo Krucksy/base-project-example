e35fabe9c891448052ac3d74e1cb077f90401c54
Simon Plumey <simon.plumey@he-arc.ch>
2022-04-09 10:43:29 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import { useMeta } from 'vue-meta'
+export default {
+  name: "AboutView",
+  setup () {
+    useMeta({
+      title: 'About page',
+      description: 'Ceci est une description de la page about',
+      htmlAttrs: { lang: 'fr', amp: true }
+    })
+  }
+}
+</script>
+
+
+<style scoped lang=scss>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script>
+import TheWelcome from '@/components/TheWelcome.vue'
+import { useMeta } from 'vue-meta'
+
+export default {
+  name: "HomeView",
+  components: {
+    TheWelcome
+  },
+  setup () {
+    useMeta({
+      title: 'Home page',
+      description: "Ceci est une description de la page d'accueil",
+      htmlAttrs: { lang: 'fr', amp: true }
+    })
+  }
+}
+</script>
+
+<style scoped lang=scss>
+
+</style>
